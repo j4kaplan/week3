4924f6c31dbb60228f737200a43c5aafe4728354
jordankaplan <j4kaplan@student.bridgew.edu>
2020-09-15 11:55:42 -0400

    simple f to c conversion

---
+++ FtoCconvert.py
@@ -0,0 +1,10 @@
+
+
+def main():
+    temp_in_c = float(input("what is the Celcius temp:"))
+    farenheit_temp = temp_in_c * 1.8 +32
+    print (f"{temp_in_c} degreess in Celcius is {farenheit_temp} degrees in Farenheit")
+
+
+
+main()
