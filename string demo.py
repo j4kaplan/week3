79c8f26d064ae7bf58354107554584e041b5b73e
jordankaplan <j4kaplan@student.bridgew.edu>
2020-09-15 11:34:45 -0400

    introducing a f-string example

--- string demo.py
+++ string demo.py
@@ -1,6 +1,8 @@
 def main():
+    # Now lets find out who you are
+    name = input("what is your name:")
     age = 5
-    str_demo = f"wow you are {age}, you are growing up"
+    str_demo = f"wow {name} you are {age}, you are growing up"
     print (str_demo)
 
 
